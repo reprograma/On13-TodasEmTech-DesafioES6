26f1558cf1b41728715e5f316b7d5cf145947eb4
Ylana Portela <ylanaportela2000@gmail.com>
2021-11-10 22:21:41 -0300

    consertando a requisição e adicionando funcionalidades

--- ylana-portela/assets/js/script.js
+++ ylana-portela/assets/js/script.js
@@ -1,29 +1,42 @@
+const divInformations = document.querySelector('.informations')
+const divError = document.querySelector('.error')
 const imageUser = document.querySelector(".user-image");
 const titleUser = document.querySelector(".user-name");
 const usernameUser = document.querySelector(".user-username");
 const descriptionUser = document.querySelector(".user-description");
-const input = document.querySelector("#input");
-
-
-
-    
-
+const input = document.querySelector("#input");    
 
 const eventInput = async (event) => {
-
-    // const url = 
-
-    let search = input.value;
-    event.preventDefault();
-    const result = await fetch(`https://api.github.com/users/${search}`)
-    const response = await result.json()
-    console.log(response)
-
-    imageUser.setAttribute('src', response.avatar_url)
-    titleUser.textContent = response.name
-    usernameUser.textContent = response.login
-    descriptionUser.textContent = response.bio
     
+    let search = input.value;
+
+    const response = await fetch(`https://api.github.com/users/${search}`)
+    const result = await response.json()
+
+    if (response.status == 200){
+
+        divInformations.style.display = "block"
+        imageUser.setAttribute('src', result.avatar_url)
+        titleUser.textContent = result.name
+        usernameUser.textContent = result.login
+        descriptionUser.textContent = result.bio
+    }
+    else{
+        divInformations.style.display = "none"
+        divError.style.display = "flex"
+    }
+
+    
+    // try {
+    //     const response = await fetch(`https://api.github.com/users/${search}`)
+    //     const result = await response.json()
+    //     imageUser.setAttribute('src', result.avatar_url)
+    //     titleUser.textContent = result.name
+    //     usernameUser.textContent = result.login
+    //     descriptionUser.textContent = result.bio
+    // }catch (error) {
+    //     alert(error)
+    // }       
 }
 
 addEventListener("change", eventInput)    
