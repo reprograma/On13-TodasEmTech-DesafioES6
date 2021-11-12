7d8388b65b7ae5d24d205beab796d2566dc6db63
Daniele Marques <danirbd13@hotmail.com>
2021-11-11 23:28:33 -0300

    Finalizada atividade de popular a pagina com informações da api do Github e mostrar que usuario não foi encontrado, caso ele não exista no banco de dados

--- entrega-projeto/Daniele-Sâmara/assets/js/script.js
+++ entrega-projeto/Daniele-Sâmara/assets/js/script.js
@@ -1,8 +1,14 @@
-const user = document.querySelector("input.input");
-const usuarios = user;
-const fotoPerfil = document.querySelector("img.foto-perfil");
+const usuarios = document.querySelector("input.input");
+const nome = document.querySelector(".name");
+const fotoPerfil = document.querySelector(".foto-perfil");
 const botao = document.querySelector(".botao");
-
+const nav = document.querySelector(".nav");
+const bio = document.querySelector(".description");
+const seguidores = document.querySelector(".seguidores");
+const repositorios = document.querySelector(".repositorios");
+const username = document.querySelector(".username");
+const containerNotfound = document.querySelector(".container-notfound");
+const cardIcons = document.querySelector(".card-icons");
 const myHeaders = new Headers();
 const myInit = {
   method: "GET",
@@ -10,11 +16,21 @@ const myInit = {
 };
 
 botao.addEventListener("click", function req() {
-  console.log(usuarios.value);
+  // console.log(usuarios.value);
   fetch(`https://api.github.com/users/${usuarios.value}`)
     .then((response) => response.json())
     .then((item) => {
-      fotoPerfil.setAttribute = ("src", item.avatar_url);
+      console.log(item);
+      fotoPerfil.src = item.avatar_url;
+      nome.textContent = item.name;
+      bio.textContent = item.bio;
+      seguidores.textContent = item.followers;
+      repositorios.textContent = item.public_repos;
+      username.textContent = item.login;
+      if (item.message == "Not Found") {
+        containerNotfound.style.display = "flex";
+        cardIcons.style.display = "none";
+      }
     })
     .catch((err) => {
       console.log(err);
