27b72c6bc90f322f19c987cd0e5dd8c69d734aa3
dardaniacoimbra <dardaniacoimbra@gmail.com>
2021-12-06 21:33:12 -0300

    alteração de formatação no projeto.

--- entrega-projeto/Dardânia-Camargos/assets/js/script.js
+++ entrega-projeto/Dardânia-Camargos/assets/js/script.js
@@ -11,28 +11,28 @@ const containerNotfound = document.querySelector(".container-notfound");
 const cardIcons = document.querySelector(".card-icons");
 const myHeaders = new Headers();
 const myInit = {
-  method: "GET",
-  headers: myHeaders,
+    method: "GET",
+    headers: myHeaders,
 };
 
 botao.addEventListener("click", function req() {
-  // console.log(usuarios.value);
-  fetch(`https://api.github.com/users/${usuarios.value}`)
-    .then((response) => response.json())
-    .then((item) => {
-      console.log(item);
-      fotoPerfil.src = item.avatar_url;
-      nome.textContent = item.name;
-      bio.textContent = item.bio;
-      seguidores.textContent = item.followers;
-      repositorios.textContent = item.public_repos;
-      username.textContent = item.login;
-      if (item.message == "Not Found") {
-        containerNotfound.style.display = "flex";
-        cardIcons.style.display = "none";
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    // console.log(usuarios.value);
+    fetch(`https://api.github.com/users/${usuarios.value}`)
+        .then((response) => response.json())
+        .then((item) => {
+            console.log(item);
+            fotoPerfil.src = item.avatar_url;
+            nome.textContent = item.name;
+            bio.textContent = item.bio;
+            seguidores.textContent = item.followers;
+            repositorios.textContent = item.public_repos;
+            username.textContent = item.login;
+            if (item.message == "Not Found") {
+                containerNotfound.style.display = "flex";
+                cardIcons.style.display = "none";
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
